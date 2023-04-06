731a6f5cbe60aacacb5b45d27c219ffd5a2325cd
sowmik-sec <sowmik.sec@gmail.com>
2023-04-06 16:20:45 +0600

    verifyJWT function added

--- index.js
+++ index.js
@@ -18,6 +18,21 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send("unauthorized access");
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 const run = async () => {
   try {
     const brandCollection = client.db("houteHorology").collection("brands");
