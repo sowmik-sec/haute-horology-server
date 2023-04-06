ec3fb7abdd3c9dbaf69ebec669872b5eeedd447e
sowmik-sec <sowmik.sec@gmail.com>
2023-04-06 16:08:17 +0600

    jwt api created

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 const port = process.env.PORT || 5000;
 
@@ -25,6 +26,18 @@ const run = async () => {
       const query = {};
       const brands = await brandCollection.find(query).toArray();
       res.send(brands);
+    });
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "5h",
+        });
+        return res.send({ accessToken: token });
+      }
+      res.status(403).send({ accessToken: "" });
     });
     app.post("/users", async (req, res) => {
       const user = req.body;
