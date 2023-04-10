362a3ebf89d4c7f4fd5e0a216f625fdab5397adc
sowmik-sec <sowmik.sec@gmail.com>
2023-04-10 12:21:19 +0600

    api created for payment intent

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const jwt = require("jsonwebtoken");
 
 const port = process.env.PORT || 5000;
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -127,11 +129,24 @@ const run = async () => {
       const result = await watchCollection.find(query).toArray();
       res.send(result);
     });
-    app.get("/orders/:id", async (req, res) => {
+    app.get("/orders/:id", verifyJWT, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await orderCollection.findOne(query);
       res.send(result);
+    });
+    app.post("/create-payment-intent", async (req, res) => {
+      const order = req.body;
+      const price = order.price;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntent.create({
+        currency: "usd",
+        amount: amount,
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
     app.post("/watch/buy", verifyJWT, async (req, res) => {
       const details = req.body;
